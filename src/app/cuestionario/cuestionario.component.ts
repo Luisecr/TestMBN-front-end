f3e3b20ca1ab0e060fef093de6ae7c13d8eb97f1
Javier Sánchez <jefe10jav@gmail.com>
2017-11-23 17:25:41 -0600

    JST - Se agrega componente de "Respuestas"

---
+++ src/app/cuestionario/cuestionario.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+import { RespuestaService } from '../services/respuesta.service';
+
+@Component({
+  selector: 'app-cuestionario',
+  templateUrl: './cuestionario.component.html',
+  styleUrls: ['./cuestionario.component.css']
+})
+export class CuestionarioComponent implements OnInit {
+
+  constructor(
+    private respuestaService: RespuestaService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  exampleSchema = {
+    "type": "object",
+    "properties": {
+      "respuestas": {
+        "type": "array",
+        "items": {
+          "type": "object",
+          "properties": {
+            "tecnologiaId": {
+              "type": "object",
+              "properties": {
+                "tecnologiaId": { "type": "number" },
+                "nombreTecnologia": { "type": "string" }
+              }
+            },
+            "conocimiento_teorico_div": { "type": "string" },
+            "teorico": { "type": "number" },
+            "conocimiento_practico_div": { "type": "string" },
+            "practico": { "type": "number" },
+            "conocimiento_aplicado_div": { "type": "string" },
+            "proyecto": { "type": "number" },
+            "usuarioId": { "type": "number" }
+
+          }
+        }
+      }
+    }
+  };
+
+  exampleLayout = [
+    {
+      "key": "respuestas",
+      "type": "array",
+      "title": "Califique su conocimiento en cada una de las tecnologías: ",
+      "listItems": 0,
+      "items": [{
+        "type": "div",
+        "displayFlex": false,
+        "flex-direction": "row",
+        "items": [
+          { "key": "respuestas[].tecnologiaId.tecnologiaId", "type": "number", "condition": "false" },
+          { "key": "respuestas[].tecnologiaId.nombreTecnologia", "type": "text", title: 'Tecnología', readonly: true },
+          { "key": "respuestas[].conocimiento_teorico_div", "type": "section", title: 'Conocimiento teórico' },
+          { "key": "respuestas[].teorico", "type": "range" },
+          { "key": "respuestas[].conocimiento_practico_div", "type": "section", title: 'Conocimiento práctico' },
+          { "key": "respuestas[].practico", "type": "range" },
+          { "key": "respuestas[].conocimiento_aplicado_div", "type": "section", title: 'Porcentaje aplicado en proyecto real' },
+          { "key": "respuestas[].proyecto", "type": "range" }
+        ]
+      }]
+    },
+    {
+      type: 'submit', title: 'Guardar'
+    }
+  ];
+
+  exampleJsonObject = {
+    "respuestas": [
+      { "usuarioId": 1, "tecnologiaId": { "tecnologiaId": 5, "nombreTecnologia": "Java EE" } }
+      //{ "usuarioId": 1, "tecnologiaId": { "tecnologiaId": 6, "nombreTecnologia": "Java" } },
+      //{ "usuarioId": 1, "tecnologiaId": { "tecnologiaId": 7, "nombreTecnologia": "CSS" } },
+    ]
+  };
+
+
+  /*exampleSchema = {
+    "type": "object",
+    "properties": {
+      "cuestionario": { "type": "string" },
+      "conocimiento_teorico_div": { "type": "string" },
+      "teorico": { "type": "number" },
+      "conocimiento_practico_div": { "type": "string" },
+      "practico": { "type": "number" },
+      "conocimiento_aplicado_div": { "type": "string" },
+      "proyecto": { "type": "number" },
+      "usuarioId": { "type": "number" },
+      "tecnologiaId": { "type": "number" }
+    }
+  };*/
+
+  /*exampleLayout = [
+    { "key": "cuestionario", "type": "div", title: 'Tecnología: Angular2' },
+    { "key": "conocimiento_teorico_div", "type": "section", title: 'Conocimiento teórico' },
+    { "key": "teorico", "type": "range" },
+    { "key": "conocimiento_practico_div", "type": "section", title: 'Conocimiento práctico' },
+    { "key": "practico", "type": "range" },
+    { "key": "conocimiento_aplicado_div", "type": "section", title: 'Porcentaje aplicado en proyecto real' },
+    { "key": "proyecto", "type": "range" },
+    { "key": "usuarioId", "type": "number", "condition": "false"},
+    { "key": "tecnologiaId", "type": "number", "condition": "false"},
+    { type: 'submit', title: 'Guardar' },
+  ];*/
+
+  //exampleJsonObject = { usuarioId: 1, tecnologiaId: 5 };
+  //[(ngModel)]="exampleJsonObject"
+
+  exampleOnSubmitFn(formData) {
+    console.log("Datos a enviar===>");
+    console.log(formData.respuestas[0]);
+    this.respuestaService.create(formData.respuestas[0]).then(respuesta => { console.log(respuesta); });
+  };
+
+
+
+}
---
+++ src/app/services/respuesta.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Respuesta } from '../entities/respuesta';
+import { FormGroup } from '@angular/forms';
+import { Headers, Http } from '@angular/http'
+import 'rxjs/add/operator/toPromise';
+
+
+@Injectable()
+export class RespuestaService {
+    private respuestasUrl = 'http://localhost:8080/respuestas';
+    private console: Console;
+    private headers = new Headers({ 'Content-Type': 'application/json' })
+
+
+    constructor(
+        private http: Http
+    ) { }
+
+    create(respuesta): Promise<Respuesta> {
+        console.log("Registrando respuesta====>");
+        console.log(respuesta);
+        return this.http
+            .post(this.respuestasUrl, respuesta, { headers: this.headers })
+            .toPromise()
+            .then(res => res.json() as Respuesta)
+            .catch(this.handleError);
+    }
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
+}
